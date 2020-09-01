9e356e3582d5146186f8f12ba44b16b5cf615398
Brett Slaski <brett@thatconference.com>
2020-09-01 13:32:30 -0500

    date forge for FireStore dates

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import securityImpl from './security';
 import graphImpl from './graphql';
 import middlewareImpl from './middleware';
 import eventsImpl from './events';
+import utilityImpl from './utility';
 
 export const security = securityImpl;
 export const graph = graphImpl;
 export const middleware = middlewareImpl;
 export const events = eventsImpl;
+export const utility = utilityImpl;
---
+++ src/utility/firestoreDateForge.js
@@ -0,0 +1,80 @@
+// Date correction for Dates coming out of FireStore.
+// Converts FireStore Timestamp to JavaScript Date type
+import debug from 'debug';
+
+const dlog = debug('that:api:dateforge');
+
+function dateForge(date) {
+  let result;
+
+  if (typeof date === 'object') {
+    // either Date or Timestamp
+    if (date.toDate) {
+      // Firestore Timestamp type
+      result = date.toDate();
+    } else if (date.getTime()) {
+      // JS Date type
+      result = date;
+    }
+  } else if (typeof date === 'string') {
+    result = new Date(date);
+  }
+
+  return result;
+}
+
+function sessions(session) {
+  dlog('sessions');
+  const sessionOut = session;
+  if (session.createdAt) sessionOut.createdAt = dateForge(session.createdAt);
+  if (session.lastUpdatedAt)
+    sessionOut.lastUpdatedAt = dateForge(session.lastUpdatedAt);
+  if (session.startTime) sessionOut.startTime = dateForge(session.startTime);
+
+  return sessionOut;
+}
+
+function members(member) {
+  dlog('members');
+  const memberOut = member;
+  if (member.createdAt) memberOut.createdAt = dateForge(member.createdAt);
+  if (member.lastUpdatedAt)
+    memberOut.lastUpdatedAt = dateForge(member.lastUpdatedAt);
+
+  return memberOut;
+}
+
+function events(event) {
+  dlog('events');
+  const eventOut = event;
+  if (event.startDate) eventOut.startDate = dateForge(event.startDate);
+  if (event.endDate) eventOut.endDate = dateForge(event.endDate);
+
+  return eventOut;
+}
+
+function votes(vote) {
+  dlog('votes');
+  const voteOut = vote;
+  if (vote.createdAt) voteOut.createdAt = dateForge(vote.createdAt);
+  if (vote.lastUpdatedAt) voteOut.lastUpdatedAt = dateForge(vote.lastUpdatedAt);
+
+  return voteOut;
+}
+
+function earnedMeritBadges(earnedBadge) {
+  dlog('earnedMeritBadges');
+  const earnedBadgeOut = earnedBadge;
+  if (earnedBadge.earnedAt)
+    earnedBadgeOut.earnedAt = dateForge(earnedBadge.earnedAt);
+
+  return earnedBadgeOut;
+}
+
+export default {
+  sessions,
+  members,
+  events,
+  votes,
+  earnedMeritBadges,
+};
