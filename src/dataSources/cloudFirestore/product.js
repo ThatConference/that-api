85fd66e17dfc4dc1a26f06d21647e8b64a544ad4
Brett Slaski <brett@thatconference.com>
2021-02-13 12:03:05 -0600

    minor dateForge bug fixes, some clean up and minor package bumps (#48)
    * minor dateForge bug fixes, some clean up and minor package bumps
    
    * change supported node version

--- src/dataSources/cloudFirestore/product.js
+++ src/dataSources/cloudFirestore/product.js
@@ -2,25 +2,11 @@ import debug from 'debug';
 import utility from '../../utility';
 
 const dlog = debug('that:api:datasources:firebase:product');
-const { dateForge, entityDateForge } = utility.firestoreDateForge;
+const { entityDateForge } = utility.firestoreDateForge;
 const forgeFields = ['createdAt', 'lastUpdateAt'];
 const productDateForge = entityDateForge({ fields: forgeFields });
 
 const collectionName = 'products';
-
-const scrubProduct = ({ product, isNew, userId }) => {
-  dlog('scrubProduct called');
-  const scrubbedProduct = product;
-  const thedate = new Date();
-  if (isNew) {
-    scrubbedProduct.createdAt = thedate;
-    scrubbedProduct.createdBy = userId;
-  }
-  scrubbedProduct.lastUpdatedAt = thedate;
-  scrubbedProduct.lastUpdatedBy = userId;
-
-  return scrubbedProduct;
-};
 
 const product = dbInstance => {
   dlog('instance created');
