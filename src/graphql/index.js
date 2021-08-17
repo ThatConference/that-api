1756ccd7452466b2279597738b495ad46cb318a6
Brett Slaski <brett@thatconference.com>
2021-08-17 10:52:21 -0500

    v2 updates, packages, husky, removed influx event lifecycle call

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,12 +1,11 @@
 import auth from './directives/auth';
 import upperCase from './directives/upperCase';
 import lowerCase from './directives/lowerCase';
-import lifecycle from './events/lifecycleEvents';
 import date from './scalars/date';
 import slug from './scalars/slug';
 
 export default {
   directives: { auth, upperCase, lowerCase },
-  events: { lifecycle },
+  events: {},
   scalars: { date, slug },
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import securityImpl from './security';
 import graphImpl from './graphql';
 import middlewareImpl from './middleware';
-import eventsImpl from './events';
 import utilityImpl from './utility';
 import datasourceImpl from './dataSources';
 import mentionsImpl from './mentions';
@@ -10,7 +9,6 @@ import constantsImpl from './constants';
 export const security = securityImpl;
 export const graph = graphImpl;
 export const middleware = middlewareImpl;
-export const events = eventsImpl;
 export const utility = utilityImpl;
 export const dataSources = datasourceImpl;
 export const mentions = mentionsImpl;
