875ec38fad970034a01fb11d9afddd362642e037
Brett Slaski <brett@thatconference.com>
2021-08-17 14:37:42 -0500

    more v2 cleanup; removal of unneeded env vars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import securityImpl from './security';
 import graphImpl from './graphql';
-import middlewareImpl from './middleware';
+// import middlewareImpl from './middleware';
+// import eventsImpl from './events';
 import utilityImpl from './utility';
 import datasourceImpl from './dataSources';
 import mentionsImpl from './mentions';
@@ -8,7 +9,8 @@ import constantsImpl from './constants';
 
 export const security = securityImpl;
 export const graph = graphImpl;
-export const middleware = middlewareImpl;
+// export const middleware = middlewareImpl;
+// export const events = eventsImpl;
 export const utility = utilityImpl;
 export const dataSources = datasourceImpl;
 export const mentions = mentionsImpl;
