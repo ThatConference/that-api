875f98e9a5c34f0b76ed1d1d92b69cb61592201b
Brett Slaski <brett@thatconference.com>
2021-10-07 09:29:19 -0500

    fix new lint rule errors

--- src/graphql/directives/upperCase.js
+++ src/graphql/directives/upperCase.js
@@ -3,15 +3,13 @@
 /* eslint-disable no-underscore-dangle */
 
 import { SchemaDirectiveVisitor } from 'apollo-server';
-import debug from 'debug';
-
-const dlog = debug('that:api:directive:uppercase');
 
 class UpperCaseDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     // eslint-disable-next-line no-undef
     const { resolve = defaultFieldResolver } = field;
-    field.resolve = async function(...args) {
+    // eslint-disable-next-line func-names
+    field.resolve = async function (...args) {
       const result = await resolve.apply(this, args);
       if (typeof result === 'string') {
         return result.toUpperCase();
