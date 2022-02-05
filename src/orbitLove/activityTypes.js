932899bfb4c28b220c0c31e435628926444bf090
Brett Slaski <brett@thatconference.com>
2022-02-05 16:31:02 -0600

    Add orbit love api. tested sessions

--- src/orbitLove/activityTypes.js
+++ src/orbitLove/activityTypes.js
@@ -1,15 +1,96 @@
 // List of activity types THAT uses in Orbit.love integration
 
+/**
+ * **Property Key**
+ * typeKey – Orbit Action Type key value used
+ * action – The name of the action, same as function name called
+ * actionText – Text to use in descriptions for action taken
+ * title - Orbit Activty title to use. Values in `~` are replaced
+ */
+
 export default {
-  activity: {
-    create: 'that:activity:create',
-    attendAt: 'that:activity:attend:at',
-    attendOn: 'that:activity:attend:on',
+  session: {
+    create: () => ({
+      typeKey: 'that:session:create',
+      action: 'create',
+      actionText: 'created',
+      title: '~name~ created new session ~title~',
+    }),
+    createOpenSpace: () => ({
+      typeKey: 'that:session:create:openspace',
+      action: 'createOpenSpace',
+      actionText: 'Created open space',
+      title: '~name~ created open space session ~title~',
+    }),
+    submit: () => ({
+      typeKey: 'that:session:submit',
+      action: 'submit',
+      actionText: 'submitted',
+      title: '~name~ submitted session for ~event~ ',
+    }),
+    attendAt: () => ({
+      typeKey: 'that:session:attend:at',
+      action: 'attendAt',
+      actionText: 'attended AT',
+      title: 'Attended AT session ~title~',
+    }),
+    attendOn: () => ({
+      typeKey: 'that:session:attend:on',
+      action: 'attendOn',
+      actionText: 'attended ON',
+      title: 'Attended ON session ~title~',
+    }),
+  },
+  profile: {
+    create: () => ({
+      typeKey: 'that:profile:create',
+      action: 'create',
+      actionText: 'created',
+      title: '~name~ created THAT Profile',
+    }),
+    update: () => ({
+      typeKey: 'that:profile:update',
+      action: 'update',
+      actionText: 'updated',
+      title: '~name~ updated THAT Profile',
+    }),
+  },
+  speaker: {
+    acceptedAtThat: () => ({
+      typeKey: 'that:speaker:accept:at',
+      action: 'acceptedAtThat',
+      actionText: 'counselor AT THAT',
+      title: '~name~ accepted to present AT THAT for ~event~',
+    }),
+    acceptedOnThat: () => ({
+      typeKey: 'that:speaker:accept:on',
+      action: 'acceptedOnThat',
+      actionText: 'counselor ON THAT',
+      title: '~name~ accepted to present ON THAT for ~event~',
+    }),
+    selectedAtThat: () => ({}),
+    selectedOnThat: () => ({}),
   },
   // register or purchase?
   purchase: {
-    onthat: 'that:purchase:onticket',
-    atthat: 'that:purchase:atticket',
+    onthat: () => ({
+      typeKey: 'that:purchase:onticket',
+      action: 'onthat',
+      actionText: 'purchased ON THAT',
+      title: 'Purchased ON THAT',
+    }),
+    atthat: () => ({
+      typeKey: 'that:purchase:atticket',
+      action: 'atthat',
+      actionText: 'purchased AT THAT',
+      title: 'Created THAT Profile',
+    }),
+    membership: () => ({
+      typeKey: 'that:purchase:membership',
+      action: 'membership',
+      actionText: 'purchased membership',
+      title: ``,
+    }),
   },
   favorite: {
     activity: 'that:favorite:activity',
@@ -24,17 +105,19 @@ export default {
   news: {
     create: 'that:news:create',
   },
-  speaker: {
-    atthat: 'that:speaker:at',
-    onthat: 'that:speaker:on',
-  },
   signups: {
-    newsletter: 'that:newsletter:signup',
-    slack: 'that:slack:signup',
-  },
-  profile: {
-    create: 'that:profile:create',
-    update: 'that:profile:update',
+    newsletter: () => ({
+      typeKey: 'that:newsletter:signup',
+      action: 'newsletter',
+      actionText: 'newsletter',
+      title: '~email~ signed-up for THAT Newsletter',
+    }),
+    slack: () => ({
+      typeKey: 'that:slack:signup',
+      action: 'slack',
+      actionText: 'slackd',
+      title: '~email~ signed-up for THAT Slack',
+    }),
   },
   createAuth0Login: 'that:auth:create',
 };
--- src/orbitLove/orbitLoveApi.js
+++ src/orbitLove/orbitLoveApi.js
@@ -1,48 +1,212 @@
 // our  Orbit.love api sdk
+import 'dotenv/config';
 import debug from 'debug';
 import fetch from 'node-fetch';
+import { getOrbitLove } from '../envConfig';
+import dataSources from '../dataSources';
+import activityTypesSrc from './activityTypes';
 
+const { cloudFirestore } = dataSources;
+const memberStore = cloudFirestore.member;
+const eventStore = cloudFirestore.event;
 const dlog = debug('that:api:orbit-love');
+const thatusbase = 'https://that.us';
 
-export function addActivityByEmail({ activityType, payload, email }) {
-  dlog('addActivity called');
-  // https://docs.orbit.love/reference/post_-workspace-slug-activities
-}
+export const activityTypes = activityTypesSrc;
 
-/**
-π denotes required
+export default function orbitLoveApi({ firestore }) {
+  dlog('Orbit.love api instance created');
+  const { workspaceSlug, token: orbitToken, baseUrl } = getOrbitLove();
 
-description
-link
-link text
-π title
-activity type key
-occurred_at (defaults to now)
-properties {
-  key:'value',
-}
-member : { 
-  tags_to_add:'' 
-}
-idenity: { 
-  source: 'email',
-  email: '@'
-}
- 
-{
-  "description": "May 17 - Getting Started with the Orbit Model",
-  "link": "https://orbit-event.eventbrite.com/",
-  "link_text": "See the event",
-  "title": "Orbit Meetup Registration",
-  "activity_type_key": "eventbrite:registration",
-  "key": "registration-1827374",
-  "occurred_at": "2020-05-10T00:00:00Z",
-  "member": {
-    "tags_to_add": "Attendee"
-  },
-  "identity": {
-    "source": "email",
-    "email": "patrick@orbit.love"
+  function getThatMemberData(user) {
+    return memberStore(firestore).find(user.sub);
   }
+  function getThatEventData(eventId) {
+    return eventStore(firestore).get(eventId);
+  }
+
+  function sendOrbitPostRequest({ payload, uriAction = 'activities' }) {
+    dlog('Orbit post request');
+    const headers = {
+      Authorization: `Bearer ${orbitToken}`,
+      'Content-type': 'application/json',
+    };
+    const url = `${baseUrl}/${workspaceSlug}/${uriAction}`;
+
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(payload),
+      headers,
+    })
+      .then(res => {
+        if (res.ok) return res.json();
+        if (res.status === 422)
+          return {
+            statusText: res.statusText,
+            status: 422,
+            err: res.json(),
+          };
+        throw new Error('Request Error:', res.statusText);
+      })
+      .then(json => {
+        if (json.err)
+          throw new Error(`${json.status}:${json.statusText}:${json.err}`);
+        return json;
+      });
+  }
+
+  // API Functions
+  return {
+    async addSessionActivity({ activityType, user, session, event }) {
+      dlog('addSessionActivity called for %s', user.sub);
+
+      let thatMember = user;
+      if (!thatMember.email) {
+        thatMember = await getThatMemberData(user.sub);
+      }
+      const { firstName, lastName, profileSlug, email } = thatMember;
+      const title = activityType.title
+        .replace('~name~', `${firstName} ${lastName}`)
+        .replace('~title~', session.title || 'N/A')
+        .replace('~event~', event.slug);
+      let description = `${activityType.actionText} session "${session.title}" of type ${session.type}`;
+      if (event?.slug) description += ` for ${event.slug}.`;
+      const payload = {
+        identity: {
+          source: 'email',
+          email,
+        },
+        activity: {
+          title,
+          description,
+          activity_type_key: activityType.typeKey,
+          link: `${thatusbase}/activities/${session.id}`,
+          link_text: 'activity',
+          properties: {
+            sessionType: session.type,
+            event: event?.slug || 'not available',
+            firstName,
+            lastName,
+            profileSlug,
+          },
+        },
+      };
+
+      return sendOrbitPostRequest({ payload });
+    },
+    addProfileActivity({ activityType, member }) {
+      dlog('addProfileActivty called for %s', member.id);
+
+      const { firstName, lastName, profileSlug, email, canFeature } = member;
+      const title = activityType.title.replace(
+        '~name~',
+        `${firstName} ${lastName}`,
+      );
+      const description = `${firstName} ${lastName} ${activityType.actionText} their THAT profile. (canFeature: ${canFeature})`;
+      const payload = {
+        identity: {
+          source: 'email',
+          email,
+        },
+        activity: {
+          title,
+          description,
+          activity_type_key: activityType.typeKey,
+          link: `${thatusbase}/members/${profileSlug}`,
+          link_text: 'Profile',
+        },
+        member: {
+          name: `${firstName} ${lastName}`,
+        },
+        properties: {
+          profileSlug: profileSlug || '',
+          canFeature: canFeature || '',
+        },
+      };
+
+      return sendOrbitPostRequest({ payload });
+    },
+    async addPurchaseActivity({ activityType, member, order }) {
+      // Purchases handled in Brinks.
+      // This will be called from an event handler there.
+      dlog('addPurchaseActivity called for %s', member.id);
+      throw new Error('addPurchaseActivity not implemented');
+    },
+    async addSpeakerActivity({ activityType, member, order }) {
+      // We identify speakers at two points
+      // 1. When they accept to speak and receive ticket (order.type>speaker)
+      // 2. When they create a THAT Activity??
+      dlog('addSpeakerActivity called for %s', member.id);
+
+      const { email, firstName, lastName, profileSlug } = member;
+      const thatEvent = await getThatEventData(order.event);
+      const title = activityType.title
+        .replace('~name~', `${firstName} ${lastName}`)
+        .replace('~event~', thatEvent.slug);
+
+      const payload = {
+        identity: {
+          source: 'email',
+          email,
+        },
+        activity: {
+          title,
+          description: title,
+          activity_type_key: activityType.typeKey,
+          link: `${thatusbase}/members/${profileSlug}`,
+          link_text: 'Profile',
+        },
+        member: {
+          name: `${firstName} ${lastName}`,
+          tags_to_add: 'speaker',
+        },
+        properties: {
+          eventSlug: thatEvent.slug,
+          event: thatEvent.name,
+          firstName,
+          lastName,
+          profileSlug,
+        },
+      };
+
+      return sendOrbitPostRequest({ payload });
+    },
+    addFavoriteActivity({ activityType, user, favorite }) {
+      dlog('addFavoriteActivity called');
+      throw new Error('addFavoriteActivity not implemented');
+    },
+    addSignUpActivity({ activityType, email }) {
+      // Sign-up activity isn't really done through our api so the
+      // thought here is that we will use a webhook (e.g. that-api-webhooks)
+      dlog('addSignUpActivity called for %s', email);
+      if (!email) throw new Error('email parameter is required');
+      if (!['newsletter', 'slack'].includes(activityType.action))
+        throw new Error(
+          `incorrect activityType, ${activityType.action}, provided`,
+        );
+      const title = activityType.title.replace('~email~', email);
+      const description = title;
+      const payload = {
+        identity: {
+          source: 'email',
+          email,
+        },
+        activity: {
+          title,
+          description,
+          activity_type_key: activityType.typeKey,
+        },
+      };
+
+      return sendOrbitPostRequest({ payload });
+    },
+    addLeadGenActivity({ activityType, user }) {
+      dlog('addLeadGenActivity called');
+      throw new Error('addLeadGenActivity is not implemented');
+    },
+    addNewsActivity({ activityType, user }) {
+      dlog('addNewsActivity called');
+      throw new Error('addNewsActicity is not implemented');
+    },
+  };
 }
- */
